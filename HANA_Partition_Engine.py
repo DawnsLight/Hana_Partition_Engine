23daef64be6df8ffb44d8a704746e24b69fd76b1
Aurora <aaron74.aurora@gmail.com>
2021-05-06 17:29:46 +0800

    initial version

--- HANA_Partition_Engine.py
+++ HANA_Partition_Engine.py
@@ -1,3 +1,32 @@
 ## Tool to do the partition in HANA DB
 ## Author: Cjiang
 ## Version: Alpha
+
+import time
+
+def timer():
+    pass
+
+
+def log_tracker():
+    pass
+
+
+def data_tracker():
+    pass
+
+
+def io_tracker():
+    pass
+
+
+def uncompressed_tracker():
+    pass
+
+
+def show_progress():
+    pass
+
+
+def hdb_partition():
+    pass
